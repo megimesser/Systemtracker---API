9fc292f0cc4f01613dc89cea9f1bed3aa46c8eae
megi-messer <heinze.obach@web.de>
2026-07-08 15:18:02 +0200

    Messagescript

--- Systemtracker/systemabfrage.py
+++ Systemtracker/systemabfrage.py
@@ -77,6 +77,7 @@ def systemabruf(befehlskette):
 
                     entries.append({
                         "uptime": parts[0],
+                        "minutes": parts[1]
 
 
                     })
