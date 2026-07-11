b40daf6c7ded73ff27e442222207b5624f83d585
megi-messer <heinze.obach@web.de>
2026-07-11 19:56:52 +0200

    Messagescript

---
+++ Datenbank/init_db.py
@@ -0,0 +1,12 @@
+from database import engine, Base
+import models  # noqa: F401 – Import nötig, damit die Modelle registriert werden
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    print("Tabellen erstellt.")
+
+
+if __name__ == "__main__":
+    init_db()
+    
