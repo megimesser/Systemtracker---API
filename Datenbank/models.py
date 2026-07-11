712beea8636a3bf5f5fffc89106d005dad05d871
megi-messer <heinze.obach@web.de>
2026-07-11 20:45:06 +0200

    Messagescript

--- Datenbank/models.py
+++ Datenbank/models.py
@@ -1,5 +1,5 @@
-from DateTime import DateTime, Timezones
-from sqlalchemy import String, Float, Integer, Datetime, Text, ForeignKey
+from datetime import datetime, timezone
+from sqlalchemy import String, Float, Integer, DateTime, Text, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import Base 
 
@@ -7,7 +7,7 @@ from database import Base
 class Position(Base):
     __tablename__ = "discinformation"
 
-    id.Mapped[int] = mapped_column(Integer, primary_key=True)
+    id:Mapped[int] = mapped_column(Integer, primary_key=True)
     filesystem: Mapped[str] = mapped_column(String(40), index = True)
     size: Mapped[str] = mapped_column(String(10), index = True)
     used: Mapped[str] = mapped_column(String(10), index = True)
