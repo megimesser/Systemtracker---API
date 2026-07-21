d5a47bb6f7493d2f38dfd27bee20c2c026c21738
megi-messer <heinze.obach@web.de>
2026-07-21 10:38:48 +0200

    Messagescript

--- Systemtracker/helper.py
+++ Systemtracker/helper.py
@@ -71,6 +71,7 @@ def docker_ps(lines,path):
     for line in lines[1:]:
         parts = line.split(",")
         print(line)
+        print(parts)
                     
        # entries.append({
         #    "container": parts[1],
