fbbe1c511500afef1d09fd182155f55a7e316d34
megi-messer <heinze.obach@web.de>
2026-07-21 08:49:05 +0200

    Messagescript

--- Datenbank/models.py
+++ Datenbank/models.py
@@ -19,10 +19,12 @@ class Datentraeger(Base):
         default=lambda: datetime.now(timezone.utc),
     )
 
+
 class Ram(Base):
     __tablename__ = "raminformation"
 
     id:Mapped[int] = mapped_column(Integer, primary_key = True)
+    marker: Mapped[str] = mapped_column(String(300), index = True)
     total: Mapped[str] = mapped_column(String(300), index = True)
     used: Mapped[str] = mapped_column(String(300), index = True)
     free: Mapped[str] = mapped_column(String(300), index = True)
@@ -39,7 +41,7 @@ class Sysinfo(Base):
     id:Mapped[int] = mapped_column(Integer, primary_key = True)
     uptime: Mapped[str] = mapped_column(String(300), index = True)
     users: Mapped[str] = mapped_column(String(300), index = True)
-    load_average: Mapped[str] = mapped_column(String(300), index = True)
+    load_averages: Mapped[str] = mapped_column(String(300), index = True)
     erfasst_am: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         default=lambda: datetime.now(timezone.utc),
@@ -52,6 +54,8 @@ class Docker(Base):
     id:Mapped[int] = mapped_column(Integer, primary_key = True)
     image: Mapped[str] = mapped_column(String(300), index = True)
     runtime: Mapped[str] = mapped_column(String(300), index = True)
+    status: Mapped[str] = mapped_column(String(300), index = True)
+
     erfasst_am: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         default=lambda: datetime.now(timezone.utc),
@@ -59,7 +63,7 @@ class Docker(Base):
 
 
 class Temp(Base):
-    __tablename__ = "Temperatur"
+    __tablename__ = "Temperaturinformation"
 
     id:Mapped[int] = mapped_column(Integer, primary_key = True)
     temp: Mapped[str] = mapped_column(String(300), index = True)
@@ -67,6 +71,33 @@ class Temp(Base):
         DateTime(timezone=True),
         default=lambda: datetime.now(timezone.utc),
     )
+
+
+class Proc(Base):
+    __tablename__ = "Prozessinformation"
+
+    id:Mapped[int] = mapped_column(Integer, primary_key = True)
+    user: Mapped[str] = mapped_column(String(300), index = True)
+    pid: Mapped[str] = mapped_column(String(300), index = True)
+    Mem: Mapped[str] = mapped_column(String(300), index = True)
+    erfasst_am: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+    )
+
+
+class Cpu(Base):
+    __tablename__ = "Cpuinformation"
+
+    id:Mapped[int] = mapped_column(Integer, primary_key = True)
+    user: Mapped[str] = mapped_column(String(300), index = True)
+    pid: Mapped[str] = mapped_column(String(300), index = True)
+    CPU: Mapped[str] = mapped_column(String(300), index = True)
+    erfasst_am: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+    )
+
 
 
 
