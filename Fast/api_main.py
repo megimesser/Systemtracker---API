d963237ec0c06cf2d06cac01173e2edf00858798
megi-messer <heinze.obach@web.de>
2026-07-25 21:42:49 +0200

    test

--- Fast/api_main.py
+++ Fast/api_main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-@app.get("/")
-def root():
+@app.get("/") #-- Method mit Path
+async def root():  #-- Funktion
     return {"Test"}
+
+# pip install fastapi --all  - installiert mit uvicorn
+
+# Start uvicorn main:app - läuft über Port 8000
+# Der Status wird immer von oben nach unten durchgerendert - 
+# Die erste Pathoperation wird immer als erstes genommen 
+
+
+#post 
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"message": payload}
--- test.py
+++ test.py
@@ -15,3 +15,5 @@ if match:
     print(davor)
     print(danach)
 
+
+
