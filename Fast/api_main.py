b12860e502675880235e6c757d52e0419f628f36
megi-messer <heinze.obach@web.de>
2026-07-27 15:32:05 +0200

    message

--- Fast/api_main.py
+++ Fast/api_main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -18,3 +20,34 @@ async def root():  #-- Funktion
 def create_posts(payload: dict = Body(...)):
     print(payload)
     return {"message": payload}
+
+
+# Datenvalidierung über pydantic
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title":"test", "id":3}
+]
+
+# Der Inhalt von Post wird hier validiert
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post) # Pydantic Model
+    print(new_post.dict()) # Python Dictionary
+    return {"message": new_post}
+
+
+# Post retrieven # Id ist hier der Pathparameter
+@app.get("/posts/{id}")
+#Die id muss hier als Integer mitgegeben werden ansonßten bekommt man einen Type Error 
+def get_posts(id: int):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+    
+    return {"message": "Post not found"}
